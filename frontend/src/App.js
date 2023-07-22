d3cdfb631325b8cbc2363c017484efbaa63adf6a
Alejandro ZZ <ale0720199@gmail.com>
2023-07-22 15:21:05 -0400

    journal page's design done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
 import Dashboard from "./pages/Dashboard/Dashboard";
-
+import Journal from "./pages/Journal/Journal";
 function App() {
   return (
     <div className="App">
@@ -15,6 +15,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/journal" element={<Journal />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ frontend/src/pages/Journal/Journal.js
@@ -0,0 +1,39 @@
+import "./Journal.css";
+
+export default function Journal() {
+  return (
+    <div className="bg">
+      <div className="container journal__container">
+        <div>&larr; Go Back</div>
+        <div className="journal__content">
+          <h2>Title Name</h2>
+          <h3>2023-06-24</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Dignissimos, tempora accusantium. Facilis, molestiae nam! Ex culpa
+            debitis doloremque sint, nam unde animi possimus accusamus iste
+            pariatur corporis, nulla fugit dignissimos voluptatibus ea
+            architecto maxime autem eveniet incidunt? Dolores officiis aperiam
+            quaerat. Consequuntur velit blanditiis adipisci soluta
+            exercitationem nam suscipit ut quis quam ipsam, maxime voluptates?
+            Reprehenderit commodi eos, omnis quo ex aliquid alias! Enim sapiente
+            quaerat quas sed quisquam dolores minus eaque autem, ad deleniti
+            aliquid odit molestiae provident nulla nihil vel corrupti id ab
+            corporis dicta nemo cupiditate blanditiis voluptatum. Doloribus
+            libero modi autem temporibus non, a asperiores dicta laboriosam
+            error numquam nam quos beatae possimus hic commodi harum, quidem
+            consectetur neque praesentium voluptatibus voluptatem quisquam,
+            debitis laudantium. Nobis inventore dignissimos sit blanditiis
+            voluptates ipsam corporis sint a cumque! Ex molestias a recusandae,
+            quisquam incidunt quas perferendis fugiat dicta velit, repellendus
+            delectus voluptates excepturi maiores optio corporis debitis nemo.
+            debitis laudantium. Nobis inventore dignissimos sit blanditiis
+            voluptates ipsam corporis sint a cumque! Ex molestias a recusandae,
+            quisquam incidunt quas perferendis fugiat dicta velit, repellendus
+            delectus voluptates excepturi maiores optio corporis debitis nemo.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
